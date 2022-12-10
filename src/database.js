6b7e3b570cbb603c3d6f567b4e4526999b0bd94c
TVM23 <99370198+TVM23@users.noreply.github.com>
2022-12-09 21:06:55 -0600

    g

--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-mongoose.set('strictQuery', false);
+/* mongoose.set('strictQuery', false);*/
 
 const conectarDB = async () => {
     try {
         /* const db = await mongoose.connect(process.env.MONGO_URI, { */
-        const db = await mongoose.connect('mongodb+srv://ad-t23:519.Itj@clusteritj.jsn9u39.mongodb.net/prueba', {
+        const db = await mongoose.connect('mongodb+srv://ad-t23:519.Itj@clusteritj.jsn9u39.mongodb.net/prueba?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
@@ -14,6 +14,13 @@ const conectarDB = async () => {
         console.log(`Error: ${error.message}`);
         process.exit(1); //Imprime un mensaje de error y termina el programa
     }
+
+    //get the default connection
+    const db = mongoose.connection;
+
+    //Bind connection to error event (to get notification of connection errors)
+    db.on("error", console.error.bind(console, "MongoDB connection error: "));
+    
 };
 
 export default conectarDB;
